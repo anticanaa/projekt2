3a7a79b6c72d43dcbf68287d546050bd33284bf0
Ana Antic <aantic@schueler.hakzell.at>
2024-04-17 18:11:31 +0200

    Projektdateien hinzufügen.

---
+++ App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Projekt2
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ MainPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿namespace Projekt2
+{
+    public partial class MainPage : ContentPage
+    {
+      
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+     
+    }
+
+}
